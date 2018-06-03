1372a426dcf8fab2ec2e4b179341e6c5303441f9
Dre Shannon <ldsz89@mail.missouri.edu>
2018-06-02 19:55:19 -0500

    Added sidebar

--- src/components/CharacterCreate.vue
+++ src/components/CharacterCreate.vue
@@ -1,26 +1,32 @@
 <template>
     <!-- TODO: Get rid of margin-top attr on div that is being set by Vue -->
     <div>
-        <nav>
-            <div class="nav-wrapper">
-                <a href="#" class="brand-logo">Character Creator</a>
-                <a href="#" data-activates="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
-            </div>
-        </nav>
-        <ul id="nav-mobile" class="sidenav">
-            <li class="active"><router-link class="btn" to="/charactercreate">Create Character</router-link></li>
+        <ul id="nav-mobile" class="sidenav sidenav-fixed" style="overflow: auto;">
+            <li class="logo center"><img src="../assets/circled-notch.png" alt="" style="height: 150px;" /></li>
+            <!-- <li class="active"><router-link class="" to="/charactercreate">Characters</router-link></li> -->
+            <router-link class="" tag="li" to="/charactercreate"><a>Characters</a></router-link>
+            <li><router-link class="" to="/charactercreate">Games</router-link></li>
+            <li><a href="google.com">Google</a></li>
         </ul>
-        <div class="containter">
-
-        </div>
-        <button v-on:click="logout">Logout</button>
-        {{ name }}
+        <main style="padding-left: 300px;">
+            <div class="container"></div>
+            <nav>
+                <div class="nav-wrapper">
+                    <a href="#" class="brand-logo">Character Creator</a>
+                    <a href="#" data-activates="nav-mobile" class="sidenav-trigger"><i class="material-icons">menu</i></a>
+                </div>
+            </nav>
+            <button v-on:click="logout">Logout</button>
+            <div class="fixed-action-btn">
+                <router-link :to="{ name: '', params: {} }"></router-link>
+            </div>
+        </main>
     </div>
 </template>
 
 <script>
     $(document).ready(function() {
-        $('.sidenav-trigger').sidenav();
+        $('.sidenav').sidenav();
     })
 
     import firebase from 'firebase'
--- src/components/UserAuth.vue
+++ src/components/UserAuth.vue
@@ -4,54 +4,6 @@
         <div class="row">
             <div id="firebaseui-auth-container"></div>
         </div>
-        <!-- <div class="row">
-            <div class="col s12">
-                <ul class="tabs">
-                    <li class="tab col s6">
-                        <a href="#signup" class="active">Sign Up</a>
-                    </li>
-                    <li class="tab col s6">
-                        <a href="#signin">Sign In</a>
-                    </li>
-                </ul>
-            </div>
-            <div id="signup" class="col s12">
-                <h2>Create new account</h2>
-                <div class="row">
-                    <form class="col s12">
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email-signup" type="email" class="validate" v-model="email" />
-                                <label for="email-signup">Email</label>
-                            </div>
-                            <div class="input-field col s12">
-                                <input id="password-signup" type="password" class="validate" v-model="password" />
-                                <label for="password-signup">Password</label>
-                            </div>
-                            <button type="submit" class="btn col s12">Sign Up</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-            <div id="signin" class="col s12">
-                <h2>Sign in to existing account</h2>
-                <div class="row">
-                    <form class="col s12" @submit.prevent="signIn">
-                        <div class="row">
-                            <div class="input-field col s12">
-                                <input id="email" type="email" class="validate" v-model="email" />
-                                <label for="email">Email</label>
-                            </div>
-                            <div class="input-field col s12">
-                                <input id="password" type="password" class="validate" v-model="password" />
-                                <label for="password">Password</label>
-                            </div>
-                            <button type="submit" class="btn col s12">Sign In</button>
-                        </div>
-                    </form>
-                </div>
-            </div>
-        </div> -->
     </div>
 </template>
 
